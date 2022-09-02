1f14d5e34882d1da0987e72ec39c512daf956bb9
Sebastian <sebastian.krahnke@stud-mail.uni-wuerzburg.de>
2022-09-02 14:31:06 +0200

    test commit

---
+++ desktop/src/com/sk/editor/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.sk.editor;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.sk.editor.Editor;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("Editor Redone");
+		new Lwjgl3Application(new Editor(), config);
+	}
+}
